ac6c4f709c123f13ba79341a401674e3b2955169
janginkyu <janginkyu.larr@gmail.com>
2023-09-11 14:01:13 +0900

    fix amax/alphamax, changed topic name

--- joy2ref.cpp
+++ joy2ref.cpp
@@ -20,7 +20,7 @@ public:
     joy2refConvertor()
     : Node("joy2accref")
     {
-        publisher_ = this->create_publisher<pcbf_msgs::msg::VectorXd>("acc_ref",10);
+        publisher_ = this->create_publisher<pcbf_msgs::msg::VectorXd>("ref_input",10);
         timer_ = this->create_wall_timer(
             100ms, std::bind(&joy2refConvertor::accref_callback, this));
 
@@ -39,8 +39,8 @@ private:
         double a = msg->axes[1];
         double b = msg->axes[0];
 
-        acc_ref_ = 0.25 * a;
-        angacc_ref_ = 0.25 * b;
+        acc_ref_ = 0.1 * a;
+        angacc_ref_ = 0.5 * b;
         
         stop_ = msg->buttons[2];
     }
@@ -50,12 +50,14 @@ private:
         // std_msgs::msg::Float64MultiArray message;
         pcbf_msgs::msg::VectorXd message;
         message.header.stamp = this->get_clock()->now();
-        message.data.push_back(0);
-        message.data.push_back(0);
+        if(!stop_) {
+            message.data.push_back(0);
+            message.data.push_back(0);
+            message.data[0] = acc_ref_;
+            message.data[1] = angacc_ref_;
+        }
         // std::this_thread::sleep_for(1s);
         // message.data = "Acc is " + std::to_string(acc_ref_) + ", Angular acc is " + std::to_string(angacc_ref_);
-        message.data[0] = acc_ref_;
-        message.data[1] = angacc_ref_;
         publisher_->publish(message);
     }
     rclcpp::TimerBase::SharedPtr timer_;
